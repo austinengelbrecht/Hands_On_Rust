cd94cc50ed96ca58f9ba0c51ffe9153f15f9a49a
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-10-25 23:50:56 -0500

    Refactor if check to use iter

--- 02_First_Steps_with_Rust/treehouse/src/main.rs
+++ 02_First_Steps_with_Rust/treehouse/src/main.rs
@@ -7,25 +7,50 @@ struct Visitor {
     greeting: String,
 }
 
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+}
+
 fn main() {
     println!("Hello, what's your name?");
 
     let name = what_is_your_name();
 
-    let visitor_list = ["bert", "steve", "fred"];
-    let mut let_them_in = false;
+    let visitor_list = [
+        Visitor::new("bert", "Hey Bert! enjoy the tree house!"),
+        Visitor::new("austin", "Hi Austin! Welcome back!"),
+        Visitor::new("fred", "Wow, who invited fred?"),
+    ];
 
-    for visitor in &visitor_list {
-        if visitor == &name {
-            let_them_in = true;
-        }
+    let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
+
+    match known_visitor {
+        Some(visitor) => visitor.greet_visitor(),
+        None => println!("You're not on the list. Please leave..."),
     }
 
-    if let_them_in {
-        println!("Welcome, to the tree house, {}", name);
-    } else {
-        println!("Sorry, you're not on the list!");
-    }
+    // let mut let_them_in = false;
+
+    // for visitor in &visitor_list {
+    //     if visitor == &name {
+    //         let_them_in = true;
+    //     }
+    // }
+
+    // if let_them_in {
+    //     println!("Welcome, to the tree house, {}", name);
+    // } else {
+    //     println!("Sorry, you're not on the list!");
+    // }
 }
 
 fn what_is_your_name() -> String {
