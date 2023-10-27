dc9223dde3b766464217f2b834fd3ee5a21ae90a
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-10-26 23:55:28 -0500

    Turn into loop to add more friends to the tree

--- 02_First_Steps_with_Rust/treehouse/src/main.rs
+++ 02_First_Steps_with_Rust/treehouse/src/main.rs
@@ -26,20 +26,27 @@ fn main() {
         println!("Hello, what's your name?");
         let name = what_is_your_name();
 
-        break;
-    }
+        let mut visitor_list = vec![
+            Visitor::new("bert", "Hey Bert! enjoy the tree house!"),
+            Visitor::new("austin", "Hi Austin! Welcome back!"),
+            Visitor::new("fred", "Wow, who invited fred?"),
+        ];
 
-    let mut visitor_list = vec![
-        Visitor::new("bert", "Hey Bert! enjoy the tree house!"),
-        Visitor::new("austin", "Hi Austin! Welcome back!"),
-        Visitor::new("fred", "Wow, who invited fred?"),
-    ];
+        let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
 
-    let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => {
+                if name.is_empty() {
+                    break;
+                } else {
+                    println!("{}, is not on the visitor list...", name);
+                    visitor_list.push(Visitor::new(&name, "New Friend!"))
+                }
+            }
+        }
 
-    match known_visitor {
-        Some(visitor) => visitor.greet_visitor(),
-        None => println!("You're not on the list. Please leave..."),
+        println!("Thanks for visiting the tree house!");
     }
 }
 
