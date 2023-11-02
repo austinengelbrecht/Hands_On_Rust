d41c04d6a863dd88e10eb7dd0af3713aac9b4dda
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-11-02 17:47:00 -0500

    prelude file data

--- 04_Design_a_Dungeon_Crawler/Rusty_Roguelike/src/main.rs
+++ 04_Design_a_Dungeon_Crawler/Rusty_Roguelike/src/main.rs
@@ -1,6 +1,11 @@
-use bracket_lib::prelude::*;
-
 mod map;
+
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+}
 
 fn main() {
     println!("Hello, world!");
