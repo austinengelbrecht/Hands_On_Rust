3a657f0496315cf72d13f6853e904c1b3a5e47b0
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2024-01-08 00:27:12 -0600

    Monster now navigate to player

--- Rusty_Roguelike/src/map.rs
+++ Rusty_Roguelike/src/map.rs
@@ -40,14 +40,16 @@ impl Map {
     }
 
     fn valid_exit(&self, loc: Point, delta: Point) -> Option<usize> {
-        let destination = loc + delta;
-
+        // (1)
+        let destination = loc + delta; // (2)
         if self.in_bounds(destination) {
+            // (3)
             if self.can_enter_tile(destination) {
-                let idx = self.point2d_to_index(destination);
+                // (4)
+                let idx = self.point2d_to_index(destination); // (5)
                 Some(idx)
             } else {
-                None
+                None // (6)
             }
         } else {
             None
@@ -60,20 +62,23 @@ impl Algorithm2D for Map {
         Point::new(SCREEN_WIDTH, SCREEN_HEIGHT)
     }
 
-    fn in_bounds(&self, pos: Point) -> bool {
+    fn in_bounds(&self, point: Point) -> bool {
         self.in_bounds(point)
     }
 }
 
 impl BaseMap for Map {
-    fn get_available_exits(&self, idx: usize) -> SmallVec<[(usize, f32); 10]> {
-        let mut exits = SmallVec::new();
-        let location = self.index_to_point2d(idx);
+    fn get_available_exits(&self, idx: usize) -> SmallVec<[(usize, f32); 10]> // (7)
+    {
+        let mut exits = SmallVec::new(); // (8)
+        let location = self.index_to_point2d(idx); // (9)
 
         if let Some(idx) = self.valid_exit(location, Point::new(-1, 0)) {
-            exits.push((idx, 1.0))
+            // (10)
+            exits.push((idx, 1.0)) // (11)
         }
         if let Some(idx) = self.valid_exit(location, Point::new(1, 0)) {
+            // (12)
             exits.push((idx, 1.0))
         }
         if let Some(idx) = self.valid_exit(location, Point::new(0, -1)) {
@@ -86,7 +91,7 @@ impl BaseMap for Map {
         exits // (13)
     }
 
-    fn get_pathing_distance(&self, _idx1: usize, _idx2: usize) -> f32 {
+    fn get_pathing_distance(&self, idx1: usize, idx2: usize) -> f32 {
         DistanceAlg::Pythagoras.distance2d(self.index_to_point2d(idx1), self.index_to_point2d(idx2))
     }
 }
