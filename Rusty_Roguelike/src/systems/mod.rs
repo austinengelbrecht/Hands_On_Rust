e28e62ceaabe52970f560b9373ed6145db078f9d
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2024-01-12 01:18:19 -0600

    render the initial fov

--- Rusty_Roguelike/src/systems/mod.rs
+++ Rusty_Roguelike/src/systems/mod.rs
@@ -15,6 +15,7 @@ mod tooltips;
 pub fn build_input_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .add_system(fov::fov_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
