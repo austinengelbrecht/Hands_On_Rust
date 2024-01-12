84e97a764368d0c3d176ea7b7f30ce54dd657525
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2024-01-12 00:41:24 -0600

    Create FOV component

--- Rusty_Roguelike/src/main.rs
+++ Rusty_Roguelike/src/main.rs
@@ -189,5 +189,5 @@ fn main() -> BError {
         .with_simple_console_no_bg(SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2, "terminal8x8.png")
         .build()?;
 
-    main_loop(context, State::new())
+    main_loop(context, State::new());
 }
