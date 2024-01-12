f4363bc8ec21b593227ee398e272d3a9676c44db
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2024-01-12 01:44:14 -0600

    Bug fix

--- Rusty_Roguelike/src/systems/movement.rs
+++ Rusty_Roguelike/src/systems/movement.rs
@@ -2,7 +2,7 @@ use crate::prelude::*;
 
 #[system(for_each)]
 #[read_component(Player)]
-#[read_component((FieldOfView))]
+#[read_component(FieldOfView)]
 pub fn movement(
     entity: &Entity,
     want_move: &WantsToMove,
@@ -14,7 +14,7 @@ pub fn movement(
     if map.can_enter_tile(want_move.destination) {
         commands.add_component(want_move.entity, want_move.destination);
 
-        if let OK(entry) = ecs.entry_ref(want_move.entity) {
+        if let Ok(entry) = ecs.entry_ref(want_move.entity) {
             if let Ok(fov) = entry.get_component::<FieldOfView>() {
                 commands.add_component(want_move.entity, fov.clone_dirty());
             }
